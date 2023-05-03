0a5415456ae23e6806dc8b72a2ed3f4e94d88926
Alexandru Barbur <alex@ctrlc.name>
2023-05-03 11:23:29 -0400

    Correctly initialize DATA and BSS segments. Pointers are hard in C.

--- platform/samd51/startup.c
+++ platform/samd51/startup.c
@@ -1,19 +1,27 @@
 // Standard Library
 #include <stdint.h>
+#include <stddef.h> // ptrdiff_t
 
 /**
- * Linker script memory pointers.
+ * Linker script symbols.
  *
- * https://stackoverflow.com/a/64271049
+ * These symbols have no size so they are void types but we can still refer
+ * to their address in memory which will be filled in from the corresponding
+ * linker script symbol.
  */
-extern uint8_t* _text_start;
-extern uint8_t* _text_end;
-extern uint8_t* _data_start;
-extern uint8_t* _data_end;
-extern uint8_t* _bss_start;
-extern uint8_t* _bss_end;
-extern uint8_t* _stack_start;
-extern uint8_t* _stack_end;
+
+// extern const void _text_start;
+// extern const void _text_end;
+
+extern const void _data_flash_start;
+extern const void _data_ram_start;
+extern const void _data_ram_end;
+
+extern const void _bss_start;
+extern const void _bss_end;
+
+// extern const void _stack_start;
+// extern const void _stack_end;
 
 /**
  * Application entry point.
@@ -33,20 +41,30 @@ void startup();
  */
 uintptr_t vectors[1] __attribute__((section(".vectors"))) = {
   (uintptr_t) startup
+  // TODO: leave space for every other vector
 };
 
 // Reset vector.
 void startup() {
-  // TODO: copy data from FLASH to ESRAM
+  // copy data from FLASH to ESRAM
+  // https://en.cppreference.com/w/c/types/ptrdiff_t
+  // NOTE: linker script aligns DATA segment boundaries to 4 bytes
+  ptrdiff_t data_size = &_data_ram_end - &_data_ram_start; // size in bytes
+  for (ptrdiff_t offset = 0; offset < data_size; offset += sizeof(uint32_t)) {
+    *(((uint32_t*) &_data_ram_start) + offset) = *(((uint32_t*) &_data_flash_start) + offset);
+  }
 
   // initialize BSS region with zeros
-  for (uint8_t* memory = _bss_start; memory < _bss_end; memory++) {
-    (*memory) = 0;
+  // https://en.cppreference.com/w/c/types/ptrdiff_t
+  // NOTE: linker script aligns BSS segment boundaries to 4 bytes
+  ptrdiff_t bss_size = &_bss_end - &_bss_start; // size in bytes
+  for (ptrdiff_t offset = 0; offset < bss_size; offset += sizeof(uint32_t)) {
+    *(((uint32_t*) &_bss_start) + offset) = 0x00;
   }
 
   // run the application
   main();
 
-  // XXX: what should we do after the application returns?
+  // XXX: what should we do if the application returns?
   while (1) { }
 }
