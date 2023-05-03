1d3b320d44806b5b034e981c6ffb64e1b52877a3
Alexandru Barbur <alex@ctrlc.name>
2023-05-03 12:13:56 -0400

    Improve build and linker scripts.

--- build.sh
+++ build.sh
@@ -1,16 +1,17 @@
 #!/bin/bash
 
-# https://gcc.gnu.org/onlinedocs/gcc/ARM-Options.html
-# https://github.com/Microsoft/uf2/blob/master/utils/uf2conv.py
+set -e
 
+# locate directories
 TOP=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
-
 SOURCE="${TOP}/code"
 BUILD="${TOP}/build"
 
+# create build directory if it doesn't exist
 mkdir -p "$BUILD"
 
 # COMPILE
+# https://gcc.gnu.org/onlinedocs/gcc/ARM-Options.html
 arm-none-eabi-gcc -mcpu=cortex-m4 -mthumb \
     -ffreestanding \
     -Wall -Wextra \
@@ -23,10 +24,7 @@ arm-none-eabi-gcc -mcpu=cortex-m4 -mthumb \
     -Og -g \
     -c -o "${BUILD}/main.o" "${SOURCE}/main.c"
 
-# XXX
-#arm-none-eabi-objdump -dhtrwC ./build/main.o
-
-# LINK
+# LINK objects into ELF binary
 # -nostdlib implies -nodefaultlibs -nostartfiles
 # -Wl,--print-map to print linker memory map
 arm-none-eabi-gcc -mcpu=cortex-m4 -mthumb \
@@ -34,12 +32,31 @@ arm-none-eabi-gcc -mcpu=cortex-m4 -mthumb \
     -Wl,--script "${TOP}/platform/samd51/SAMD51x19-UF2BOOT.ld" \
     -o "${BUILD}/main.elf" "${BUILD}/startup.o" "${BUILD}/main.o"
 
-# arm-none-eabi-objdump -dhtrwC ./build/main.elf
-
-# XXX
+# EXTRACT code from ELF binary
 arm-none-eabi-objcopy -O binary \
     "${BUILD}/main.elf" "${BUILD}/main.bin"
 
-# XXX
-#/home/alex/projects/uf2/utils/uf2conv.py --base 0x4000 --convert \
-#    --output "${BUILD}/main.uf2" "${BUILD}/main.bin"
+# CONVERT code to UF2 format for flashing with UF2 bootloader
+# https://github.com/Microsoft/uf2/blob/master/utils/uf2conv.py
+UF2CONV="/home/alex/projects/uf2/utils/uf2conv.py"
+
+if which $UF2CONV; then
+    TEXT_BASE=$(\
+        arm-none-eabi-objdump \
+            --section-headers \
+            --section='.text' \
+            --wide \
+            "${BUILD}/main.elf" \
+        | tail -n 1 \
+        | tr -s ' ' \
+        | cut -d' ' -f5)
+
+    UF2_BASE_ADDRESS="0x${TEXT_BASE}"
+    /home/alex/projects/uf2/utils/uf2conv.py --convert \
+        --base "${UF2_BASE_ADDRESS}" \
+        --output "${BUILD}/main.uf2" \
+        "${BUILD}/main.bin"
+fi
+
+# XXX: manual verification
+arm-none-eabi-objdump -dhtrwC ./build/main.elf | less
