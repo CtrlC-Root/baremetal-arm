aa1c4cbb8cce21b845fc9cd2337ab51214c49088
Alexandru Barbur <alex@ctrlc.name>
2023-05-03 09:29:53 -0400

    Initialize BSS section with zeroes.

--- platform/samd51/startup.c
+++ platform/samd51/startup.c
@@ -1,25 +1,52 @@
 // Standard Library
 #include <stdint.h>
 
+/**
+ * Linker script memory pointers.
+ *
+ * https://stackoverflow.com/a/64271049
+ */
+extern uint8_t* _text_start;
+extern uint8_t* _text_end;
+extern uint8_t* _data_start;
+extern uint8_t* _data_end;
+extern uint8_t* _bss_start;
+extern uint8_t* _bss_end;
+extern uint8_t* _stack_start;
+extern uint8_t* _stack_end;
 
-// XXX: https://stackoverflow.com/a/64271049
-// extern uint32_t _stack_top;
-
+/**
+ * Application entry point.
+ */
 extern void main();
+
+/**
+ * Reset vector.
+ */
 void startup();
 
-
+/**
+ * Vector table.
+ *
+ * Note: The first entry should be a point to the top of the stack but in this
+ * project that is automatically added by the linker script.
+ */
 uintptr_t vectors[1] __attribute__((section(".vectors"))) = {
   (uintptr_t) startup
 };
 
+// Reset vector.
 void startup() {
   // TODO: copy data from FLASH to ESRAM
-  // TODO: initialize BSS with zeros
 
-  // XXX run the program
+  // initialize BSS region with zeros
+  for (uint8_t* memory = _bss_start; memory < _bss_end; memory++) {
+    (*memory) = 0;
+  }
+
+  // run the application
   main();
 
-  // XXX
+  // XXX: what should we do after the application returns?
   while (1) { }
 }
